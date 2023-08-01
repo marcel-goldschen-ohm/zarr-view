4cd55e4609929aecbd231d493d912dc385b71b09
Marcel Goldschen-Ohm <goldschen-ohm@utexas.edu>
2023-08-01 15:30:17 -0500

    runs quick start example

--- zarrview/__main__.py
+++ zarrview/__main__.py
@@ -6,20 +6,32 @@ except ImportError:
     except ImportError:
         from PyQt5.QtWidgets import *
 
+import sys
 import zarr
 from zarrview.ZarrViewer import ZarrViewer
-import sys
+
+# example zarr hierarchy (in-memory vs on-disk should not matter)
+root = zarr.group()
+foo = root.create_group('foo')
+bar = foo.create_dataset('bar', shape=100, chunks=10)
+baz = foo.create_group('baz')
+quux = baz.create_dataset('quux', shape=200, chunks=20)
+
+# attributes for quux
+quux.attrs['a_int'] = 82
+quux.attrs['a_float'] = 3.14
+quux.attrs['a_bool'] = False
+quux.attrs['a_str'] = 'zarr-view is awesome!'
+quux.attrs['a_dict'] = {'a_child': 42}
+quux.attrs['a_list'] = [8, 4.5, True, 'hello']
 
 # create app
 app = QApplication(sys.argv)
 
-# create a zarr group
-root = zarr.group()
+# init zarr viewer widget with root of hierarchy
+viewer = ZarrViewer(root)
+viewer.show()
+viewer.setWindowTitle('ZarrViewer')
 
-# create the UI
-ui = ZarrViewer(root)
-ui.show()
-
-# run the app
-status = app.exec()
-sys.exit(status)
+# run app
+sys.exit(app.exec())
