b0021794c825abe1e7fbdba5b79b3667eb4e833f
Marcel Goldschen-Ohm <goldschen-ohm@utexas.edu>
2023-07-31 19:13:04 -0500

    non-empty default tree

--- zarrview/__main__.py
+++ zarrview/__main__.py
@@ -6,14 +6,18 @@ except ImportError:
     except ImportError:
         from PyQt5.QtWidgets import *
 
+import zarr
 from zarrview.ZarrViewer import ZarrViewer
 import sys
 
 # create app
 app = QApplication(sys.argv)
 
+# create a zarr group
+root = zarr.group()
+
 # create the UI
-ui = ZarrViewer()
+ui = ZarrViewer(root)
 ui.show()
 
 # run the app
